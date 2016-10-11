098d5fc95837bf1292da5df9d4bcbbd5bb89b272
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-10-11 18:35:04 +0900

    Version 0.0.1

--- source/get_tweet.rb
+++ source/get_tweet.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'json'
+require 'open-uri'
 
 # load settings
 setting_file = File.open("settings.json").read
@@ -13,9 +14,59 @@ client = Twitter::REST::Client.new(
 )
 
 query = "happy"
+count = 100
+lang = "en"
 since_id = nil
-result_tweets = client.search(query, count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
+begin
+  tweets = client.search(
+    query,
+    count: count,
+    lang: lang,
+    result_type: "recent",
+    exclude: "retweets",
+    since_id: since_id
+  )
+rescue Twitter::Error::TooManyRequests => Error
+  sleep error.rate_limit.reset_in + 1
+  retry
+end
 
-result_tweets.take(10).each_with_index do |tw, i|
-  puts("#{i} : #{tw.user.screen_name} : #{tw.full_text}")
+def save_image(url, index)
+  # ready filepath
+  fileName = File.basename(url)
+  dirName = "data/images/"
+  filePath = dirName << index.to_s << ".jpg"
+
+  # write image adata
+  open(filePath, 'wb') do |output|
+    open(url) do |data|
+      output.write(data.read)
+    end
+  end
+end
+
+image_meta_data = []
+index = 0
+
+tweets.take(count).each do |tw|
+  if tw.media? then
+    tw.media.each do |m|
+      if m.is_a?(Twitter::Media::Photo)
+        puts ("text: #{tw.full_text}")
+        puts ("media: #{m.media_url}")
+        image_information = {id: index,
+                             description: tw.full_text,
+                             positive: 1,
+                             negative: 0
+                            }
+        save_image(m.media_url, index)
+        image_meta_data << image_information
+        index += 1
+      end
+    end
+  end
+end
+
+open("data/image_meta_data.json", "w") do |io|
+  JSON.dump(image_meta_data, io)
 end
